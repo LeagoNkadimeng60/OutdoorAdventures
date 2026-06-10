ffa70273e61b882615ffcc829560eb2cb79390cd
LeagoNkadimeng60 <leagonkadimeng60@gmail.com>
2026-06-10 10:08:10 +0200

    new screen

--- app/src/main/java/com/example/outdooradventures/MainScreen.kt
+++ app/src/main/java/com/example/outdooradventures/MainScreen.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,20 +13,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.outdooradventures.ui.theme.OutdoorAdventuresTheme
 
-class MainActivity : ComponentActivity() {
+class MainScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             OutdoorAdventuresTheme {
-
-                Column() {
-                    Text("Campsite Commander")
-                }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
+}
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OutdoorAdventuresTheme {
+        Greeting("Android")
+    }
+}
